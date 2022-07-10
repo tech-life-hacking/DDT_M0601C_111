4d672ac7f240e472a0388329d826912fc539dae0
tech-life-hacking <techlife.hacking@gmail.com>
2022-07-10 15:34:17 +0000

    change rotate

--- motor.py
+++ motor.py
@@ -7,16 +7,23 @@ class Motor():
         self.id = ID
 
     def stop(self):
-        _, _, Speed, _, Fault_value = self.cp.Check_Motor()
-        if Speed > 0:
-            speedlist = list(range(Speed))
-            speedlist.reverse()
-        else:
-            Speed *= -1
-            speedlist = list(range(0, Speed, 2))
-            speedlist.reverse()
-            speedlist = [n*(-1) for n in speedlist]
+        try:
+            _, _, Speed, _, Fault_value = self.cp.Check_Motor()
+            if Speed > 0:
+                speedlist = list(range(Speed))
+                speedlist.reverse()
+            else:
+                Speed *= -1
+                speedlist = list(range(0, Speed, 2))
+                speedlist.reverse()
+                speedlist = [n*(-1) for n in speedlist]
 
-        for speed in speedlist:
-            mode, Current, Velocity, Angle, Fault_value = self.cp.Control_Motor(speed, self.id, 0, 0)
-            time.sleep(0.01)
+            for speed in speedlist:
+                mode, Current, Velocity, Angle, Fault_value = self.cp.Control_Motor(speed, self.id, 0, 0)
+                time.sleep(0.01)
+        except:
+            pass
+
+    def rotate(self, speed, rotatetime):
+        self.cp.Control_Motor(speed, self.id, 0, 0)
+        time.sleep(rotatetime)
